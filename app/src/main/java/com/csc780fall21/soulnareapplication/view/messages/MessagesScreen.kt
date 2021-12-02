b3e4de46ffaffbe9a90a7ee6ebc962df0e0bc5ca
Khanh Nguyen <knguyenn11@gmail.com>
2021-12-01 19:19:54 -0800

    Setup messages screen

--- app/src/main/java/com/csc780fall21/soulnareapplication/view/messages/MessagesScreen.kt
+++ app/src/main/java/com/csc780fall21/soulnareapplication/view/messages/MessagesScreen.kt
@@ -6,70 +6,142 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.filled.ChatBubbleOutline
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
-import com.csc780fall21.soulnareapplication.domain.model.Message
-import com.csc780fall21.soulnareapplication.domain.model.User
+import com.csc780fall21.soulnareapplication.data.repository.UsersRepository
+import com.csc780fall21.soulnareapplication.domain.model.*
+import com.csc780fall21.soulnareapplication.view.home.HomeViewModel
+import com.csc780fall21.soulnareapplication.view.home.HomeViewModelFactory
+import com.csc780fall21.soulnareapplication.view.likes.LikesViewModel
+import com.csc780fall21.soulnareapplication.view.likes.LikesViewModelFactory
+import com.csc780fall21.soulnareapplication.view.profile.ProfileViewModel
+import com.csc780fall21.soulnareapplication.view.profile.ProfileViewModelFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.asStateFlow
 
+@ExperimentalCoilApi
+@ExperimentalCoroutinesApi
 @Composable
-fun MessagesScreen(navController: NavController) {
-    // messages for this user
-    val messages = mutableListOf<Message>()
-    messages.add(Message("1", "1", "Hello", "Joe", "Julia", 0))
-    messages.add(Message("2", "1","Hello you", "Mike", "Julia", 0))
-    messages.add(Message("3","1", "Yo", "John", "Julia",  0))
-    messages.add(Message("4", "1","Ayy", "David", "Julia", 0))
-    messages.add(Message("5", "1","sup", "Barry", "Julia",  0))
-    messages.add(Message("6", "1","Hi there!", "Danny", "Julia",  0))
-    messages.add(Message("7", "1","heyyy", "Jason", "Julia",  0))
-    messages.add(Message("8", "1","hello", "Danny", "Julia",  0))
-    messages.add(Message("9", "1","heyyy", "Jules", "Julia",  0))
-    messages.add(Message("10", "1","hello there", "Joey", "Julia", 0))
-    messages.add(Message("11", "1","hey hey!", "Brandon", "Julia",  0))
+fun MessagesScreen(
+    navController: NavController,
+    messagesViewModel: MessagesViewModel = viewModel(
+        factory = MessagesViewModelFactory(UsersRepository())
+    ),
+    profileViewModel: ProfileViewModel = viewModel(
+        factory = ProfileViewModelFactory(UsersRepository())
+    ),
+    homeViewModel: HomeViewModel = viewModel(
+        factory = HomeViewModelFactory(UsersRepository())
+    ),
+    likesViewModel: LikesViewModel = viewModel(
+        factory = LikesViewModelFactory(UsersRepository())
+    )
+) {
+    when(val currentUser = profileViewModel.userStateFlow.asStateFlow().collectAsState().value) {
+        is OnError -> {
+            Text(text = "Please try after sometime")
+        }
 
-    // current user
-    val user = User("","Julia", "Goldberg", "", "", mutableListOf(),  mutableListOf(),  mutableListOf(),  mutableListOf())
+        is OnSuccess -> {
+            val user = currentUser.documentSnapshot?.toObject(User::class.java)
+            val youLikeUserIds = user?.youLikeUserIds
+            val youRejectUserIds = user?.youRejectUserIds
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        TopAppBar(
-            elevation = 4.dp,
-            title = {
-                Text("Messages")
-            },
-            backgroundColor =  MaterialTheme.colors.primarySurface,
-        )
+            when (val userProfiles = likesViewModel.usersStateFlow.asStateFlow().collectAsState().value) {
+                is OnError -> {
+                    Text(text = "Please try after sometime")
+                }
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentPadding = PaddingValues(
-                start = 12.dp,
-                top = 16.dp,
-                end = 12.dp,
-                bottom = 16.dp
-            ),
-        ) {
-            items(messages) { user ->
-                MessageRow(user, navController)
-                Divider(
-                    color = Color.LightGray,
-                    thickness = 1.dp,
-                )
+                is OnSuccessQuery -> {
+                    val usersThatLikeYou = userProfiles.querySnapshot?.toObjects(User::class.java)
+                    val tempUsersToShow = mutableListOf<User>()
+                    usersThatLikeYou?.forEach {
+                        if (youLikeUserIds?.contains(it.uid)!! && !youRejectUserIds?.contains(it.uid)!!) {
+                            tempUsersToShow.add(it)
+                        }
+                    }
+
+                    val usersToShow: List<User> by homeViewModel.usersToShow.observeAsState(mutableListOf())
+                    homeViewModel.updateUsersToShow(tempUsersToShow)
+
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        TopAppBar(
+                            elevation = 4.dp,
+                            title = {
+                                Text("Messages")
+                            },
+                            backgroundColor =  MaterialTheme.colors.primarySurface,
+                        )
+                        // TODO
+                        if (usersToShow.isNotEmpty()) {
+                            LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                contentPadding = PaddingValues(
+                                    start = 12.dp,
+                                    top = 16.dp,
+                                    end = 12.dp,
+                                    bottom = 16.dp
+                                ),
+                            ) {
+                                items(usersToShow) { user ->
+                                    MessageRow(user, navController)
+                                    Divider(
+                                        color = Color.LightGray,
+                                        thickness = 1.dp,
+                                    )
+                                }
+                            }
+                        } else {
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    text = "No messages yet!",
+                                    style = MaterialTheme.typography.h5,
+                                    modifier = Modifier.padding(bottom = 20.dp)
+                                )
+                                Icon(
+                                    imageVector = Icons.Filled.ChatBubbleOutline,
+                                    contentDescription = null,
+                                    tint = Color(0xFFF9844A),
+                                    modifier = Modifier.size(50.dp)
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 }
 
+@ExperimentalCoilApi
 @Composable
-fun MessageRow(model: Message, navController: NavController) {
+fun MessageRow(
+    user: User,
+    navController: NavController
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -97,15 +169,29 @@ fun MessageRow(model: Message, navController: NavController) {
             verticalArrangement = Arrangement.Center,
         ) {
             Text(
-                text = model.fromUserId,
+                text = "${user.firstName} ${user.lastName}",
                 fontSize = 14.sp,
                 fontWeight = FontWeight.Medium
             )
             Text(
-                text = model.text,
+                text = "Hi",
                 fontSize = 12.sp,
                 fontWeight = FontWeight.Light
             )
         }
+    }
+}
+
+/**
+ * References: https://github.com/raipankaj/Bookish/blob/main/app/src/main/java/com/sample/jetbooks/MainActivity.kt
+ */
+@ExperimentalCoroutinesApi
+class MessagesViewModelFactory(private val usersRepository: UsersRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MessagesViewModel::class.java)) {
+            return MessagesViewModel(usersRepository) as T
+        }
+
+        throw IllegalStateException()
     }
 }
--- app/src/main/java/com/csc780fall21/soulnareapplication/view/messages/MessagesViewModel.kt
+++ app/src/main/java/com/csc780fall21/soulnareapplication/view/messages/MessagesViewModel.kt
@@ -1,4 +1,34 @@
 package com.csc780fall21.soulnareapplication.view.messages
 
-class MessagesViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.csc780fall21.soulnareapplication.data.repository.UsersRepository
+import com.csc780fall21.soulnareapplication.domain.model.Response
+import com.csc780fall21.soulnareapplication.domain.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@ExperimentalCoroutinesApi
+class MessagesViewModel(val usersRepository: UsersRepository) : ViewModel() {
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private var _usersToShow = MutableLiveData(listOf<User>())
+    val usersToShow: LiveData<List<User>> = _usersToShow
+
+    val usersStateFlow = MutableStateFlow<Response?>(null)
+
+    init {
+        viewModelScope.launch {
+            usersRepository.getUserProfilesThatLikeYours(auth.currentUser?.uid).collect {
+                usersStateFlow.value = it
+            }
+        }
+    }
 }
